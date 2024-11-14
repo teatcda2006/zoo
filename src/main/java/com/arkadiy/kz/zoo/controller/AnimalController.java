7c34beee81875f274bd4890bb24ad8f5a05df554
teatcda2006 <155387490+teatcda2006@users.noreply.github.com>
2024-11-14 08:53:46 +0500

    repair controller

--- src/main/java/com/arkadiy/kz/zoo/controller/AnimalController.java
+++ src/main/java/com/arkadiy/kz/zoo/controller/AnimalController.java
@@ -1,9 +1,6 @@
 package com.arkadiy.kz.zoo.controller;
 
-import com.arkadiy.kz.zoo.dto.AnimalDTO;
 import com.arkadiy.kz.zoo.entity.Animal;
-import com.arkadiy.kz.zoo.facade.AnimalFacade;
-import com.arkadiy.kz.zoo.repository.AnimalRepository;
 import com.arkadiy.kz.zoo.services.AnimalService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,51 +16,37 @@ import java.util.List;
 @CrossOrigin
 public class AnimalController {
     @Autowired
-    private AnimalFacade animalFacade;
-    @Autowired
     private AnimalService animalService;
 
-    @PostMapping("/create")
-    private ResponseEntity<Object> createAnimal(@Valid @RequestBody AnimalDTO animalDTO, BindingResult bindingResult) {
-        Animal animal = animalService.createAnimal(animalDTO);
-        AnimalDTO createdAnimal = animalFacade.animalToAnimalDTO(animal);
+    @PostMapping("/")
+    private ResponseEntity<Object> createAnimal(@Valid @RequestBody Animal animalNew) {
+        Animal animal = animalService.createAnimal(animalNew);
 
-        return new ResponseEntity<>(createdAnimal, HttpStatus.OK);
+        return new ResponseEntity<>(animal, HttpStatus.OK);
     }
 
-    @GetMapping("/all")
-    private ResponseEntity<List<AnimalDTO>> getAllAnimals() {
-        List<AnimalDTO> animalDTOList = animalService.getAllAnimals().stream().map(animalFacade::animalToAnimalDTO).toList();
+    @GetMapping("/")
+    private ResponseEntity<List<Animal>> getAllAnimals() {
+        List<Animal> animalList = animalService.getAllAnimals();
 
-        return new ResponseEntity<>(animalDTOList, HttpStatus.OK);
+        return new ResponseEntity<>(animalList, HttpStatus.OK);
     }
 
-    @GetMapping("/get/{id}")
-    private ResponseEntity<AnimalDTO> getAnimalById(@PathVariable("id") String id) {
+    @GetMapping("/{id}")
+    private ResponseEntity<Animal> getAnimalById(@PathVariable("id") String id) {
         Animal animal = animalService.getAnimalById(Long.parseLong(id));
-        AnimalDTO animalDTO = animalFacade.animalToAnimalDTO(animal);
 
-        return new ResponseEntity<>(animalDTO, HttpStatus.OK);
+        return new ResponseEntity<>(animal, HttpStatus.OK);
     }
 
-    @GetMapping("/get/{name}")
-    private ResponseEntity<AnimalDTO> getAnimalByName(@PathVariable("name") String name) {
-        Animal animal = animalService.getAnimalByName(name);
-        AnimalDTO animalDTO = animalFacade.animalToAnimalDTO(animal);
-
-        return new ResponseEntity<>(animalDTO, HttpStatus.OK);
+    @PostMapping("/{id}")
+    private ResponseEntity<Animal> updateAnimal(@PathVariable("id") String id, @RequestBody Animal animal) {
+        animalService.updateAnimal(Long.parseLong(id), animal);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/gettype/{type}")
-    private ResponseEntity<AnimalDTO> getAnimalByType(@PathVariable("type") String type) {
-        Animal animal = animalService.getAnimalByType(type);
-        AnimalDTO animalDTO = animalFacade.animalToAnimalDTO(animal);
-
-        return new ResponseEntity<>(animalDTO, HttpStatus.OK);
-    }
-
-    @PostMapping("/delete/{id}")
-    private ResponseEntity<AnimalDTO> deleteAnimal(@PathVariable("id") String id) {
+    @PostMapping("/{id}")
+    private ResponseEntity<Animal> deleteAnimal(@PathVariable("id") String id) {
         animalService.deleteAnimal(Long.parseLong(id));
 
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/com/arkadiy/kz/zoo/services/AnimalService.java
+++ src/main/java/com/arkadiy/kz/zoo/services/AnimalService.java
@@ -1,12 +1,12 @@
 package com.arkadiy.kz.zoo.services;
 
-import com.arkadiy.kz.zoo.dto.AnimalDTO;
 import com.arkadiy.kz.zoo.entity.Animal;
 import com.arkadiy.kz.zoo.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnimalService {
@@ -17,12 +17,7 @@ public class AnimalService {
         this.animalRepository = animalRepository;
     }
 
-    public Animal createAnimal(AnimalDTO animalDTO) {
-        Animal animal = new Animal();
-        animal.setName(animalDTO.getName());
-        animal.setType(animalDTO.getType());
-        animal.setAge(animalDTO.getAge());
-
+    public Animal createAnimal(Animal animal) {
         return animalRepository.save(animal);
     }
 
@@ -35,17 +30,16 @@ public class AnimalService {
                 .orElseThrow();
     }
 
-    public Animal getAnimalByName(String name) {
-        return animalRepository.findAnimalByName(name)
-                .orElseThrow();
-    }
-
-    public Animal getAnimalByType(String type) {
-        return animalRepository.findAnimalByType(type)
-                .orElseThrow();
+    public Animal updateAnimal(Long id, Animal newAnimal) {
+        return animalRepository.findAnimalById(id).map(animal -> {
+            animal.setName(newAnimal.getName());
+            animal.setAge(newAnimal.getAge());
+            animal.setType(newAnimal.getType());
+            return animalRepository.save(animal);
+        }).orElseThrow(() -> new RuntimeException("Животное не найдено"));
     }
 
     public void deleteAnimal(Long id) {
-        animalRepository.findAnimalById(id);
+        animalRepository.deleteById(id);
     }
 }
